7da745db4e9b2bd3e67bcbb39492c057c0900fb4
dev-davronbek <davronbekmamarasulov07@gmail.com>
2024-05-30 23:11:00 +0500

    asd

--- assets/script/app.js
+++ assets/script/app.js
@@ -7,6 +7,11 @@ const $videoArtist = document.querySelector("#video-artist")
 const $addBgAnimation = document.querySelector(".add-animation")
 const $videoList = document.querySelector("#video-list")
 const $shuffleBtn = document.querySelector("#shuffle-btn")
+const $videoPlayline = document.querySelector("#video-playline")
+const $playedTime = document.querySelector("#played-time")
+const $playedDuration = document.querySelector("#played-duration")
+const $container = document.querySelector(".container")
+const $nav = document.querySelector(".nav")
 
 
 let videoPlaying = false;
@@ -47,9 +52,20 @@ const renewIndex = () => {
     $currentVideoElement.src = videos[currentVideoIndex].src;
     $videoTitle.innerText = videos[currentVideoIndex].title;
     $videoArtist.innerText = videos[currentVideoIndex].artist; 
+  
 }
 
-renewIndex()
+renewIndex();
+
+const checkVideoEnd = () => {
+    let  intervalCheckVideo = setInterval(() => {
+         if($currentVideoElement.currentTime === $currentVideoElement.duration){
+             playNextVideo(); 
+             clearInterval(intervalCheckVideo)
+         }
+     },1000)
+ }
+ 
 
 
 const playVideo = () => {
@@ -59,15 +75,17 @@ const playVideo = () => {
         $playBtn.firstElementChild.classList.add("hidden")
         $playBtn.lastElementChild.classList.remove("hidden")
         $addBgAnimation.classList.add("background")
-
+        movieVideoLine();
+        
+        
     }
     else{
         $currentVideoElement.pause()
         $playBtn.firstElementChild.classList.remove("hidden")
         $playBtn.lastElementChild.classList.add("hidden")
         $addBgAnimation.classList.remove("background")
-
     }
+    checkVideoEnd();
 }
 
 const playNextVideo = () => {
@@ -78,6 +96,8 @@ const playNextVideo = () => {
     renewIndex();
     videoPlaying = true; 
     $currentVideoElement.play();
+    $playBtn.firstElementChild.classList.add("hidden")
+    $playBtn.lastElementChild.classList.remove("hidden")
    
 }
 
@@ -89,41 +109,58 @@ const playPrevVideo = () => {
     renewIndex();
     videoPlaying = true;
     $currentVideoElement.play();
-
+    $playBtn.firstElementChild.classList.add("hidden")
+    $playBtn.lastElementChild.classList.remove("hidden")
 }
 
 
-const playSelectedVideo = (event) => {
-    event.preventDefault(); 
-    const videoSrc = event.target.getAttribute("href"); 
-    const videoIndex = videos.findIndex(video => video.src === videoSrc); 
-    if (videoIndex !== -1) { 
-        currentVideoIndex = videoIndex; 
-        renewIndex(); 
-        videoPlaying = true; 
-        $currentVideoElement.play(); 
-        $addBgAnimation.classList.add("background");
-        $playBtn.firstElementChild.classList.add("hidden");
-        $playBtn.lastElementChild.classList.remove("hidden");
 
-    }
+const formatTime = (s) => {  
+    let minute = Math.floor(s / 60);
+    let seconds =  Math.floor(s % 60)
+
+    return `${minute.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`
 }
 
-$videoList.querySelectorAll("a").forEach(link => {
-    link.addEventListener("click", playSelectedVideo);
-});
+const movieVideoLine = () => {
+    let videoInterval = setInterval (() => {
+        let percent = $currentVideoElement.currentTime / $currentVideoElement.duration * 100; 
+        $videoPlayline.value = percent;
+        $playedTime.innerText = isNaN($currentVideoElement.currentTime) ? "--:--" :formatTime($currentVideoElement.currentTime);
+        $playedDuration.innerText = isNaN($currentVideoElement.currentTime) ? "--:--" : formatTime($currentVideoElement.duration - $currentVideoElement.currentTime)
+        if($currentVideoElement.currentTime === $currentVideoElement.duration){
+            clearInterval(videoInterval)
+        }
+    }, 1000)
+}
 
 const randomVideo = () => {
-    let random = Math.floor(Math.random() * videos.length)
+    let random = Math.floor(Math.random() * videos.length);
+    while(currentVideoIndex === random){
+        random = Math.floor(Math.random() * videos.length)
+    }
     currentVideoIndex = random
     renewIndex()
     videoPlaying = false,
     playVideo()
 }
 
+const chnageVideoCurrentStep = (e) => {
+    let seconds = $currentVideoElement.duration * $videoPlayline.value / 100;
+    $currentVideoElement.currentTime = seconds;
+}
 
 
+$videoPlayline.addEventListener("input" , chnageVideoCurrentStep)
 $shuffleBtn.addEventListener("click" ,randomVideo)
 $playBtn.addEventListener("click" , playVideo);
 $nextBtn.addEventListener("click" , playNextVideo)
 $prevBtn.addEventListener("click" , playPrevVideo)
+$container.addEventListener("scroll" , () => {
+    if($container.scrollTop > 85){
+        $nav.classList.add("pin")
+    }
+    else{
+        $nav.classList.remove("pin")
+    }
+})
